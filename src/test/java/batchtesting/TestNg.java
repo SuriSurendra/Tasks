17668267c0cdb6c51077833923f68dfdca6fe06b
nisum <kuntrapakusurendraqa@gmail.com>
2019-11-18 11:12:30 +0530

    design the test case sequence by using sanitysuite xml

--- src/test/java/batchtesting/TestNg.java
+++ src/test/java/batchtesting/TestNg.java
@@ -1,4 +1,4 @@
-package Test;
+package batchtesting;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
@@ -13,66 +13,56 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 
-
 public class TestNg {
-  
-	
+
 	public WebDriver driver;
-	
+
 	@Test
-  public void f() 
-	{
+	public void Test() throws InterruptedException {
 		
-WebElement dropdown= driver.findElement(By.id("open_account"));
-		
+		Thread.sleep(3000);
+
+		WebElement dropdown = driver.findElement(By.id("open_account"));
+
 		dropdown.click();
-		
-		Select corpacc= new Select(dropdown);
+
+		Select corpacc = new Select(dropdown);
 		corpacc.selectByVisibleText("Corporate Account");
-		
+
 		driver.findElement(By.xpath("//input[@value='Open my Account']")).click();
-		
-		
-		WebElement city=driver.findElement(By.id("FML_USR_ADDRSS_CTY_DROP"));
-		
-		
-		List<WebElement> cityoptions= city.findElements(By.tagName("option"));
-		
-		int citycount= cityoptions.size();
-		for(int i=0; i<citycount; i++)
-		{
-			WebElement cityoption= cityoptions.get(i);
-			String cityname= cityoption.getText();
-			
-			WebElement state= driver.findElement(By.id("FML_USR_ADDRSS_STTE_DIS"));
-			
-			String statename= state.getAttribute("punjab");
-			
-			System.out.println(cityname+"   ==>     "+statename);
-			
-			if(state.equals("punjab"))
-			{
+
+		WebElement city = driver.findElement(By.id("FML_USR_ADDRSS_CTY_DROP"));
+
+		List<WebElement> cityoptions = city.findElements(By.tagName("option"));
+
+		int citycount = cityoptions.size();
+		for (int i = 0; i < citycount; i++) {
+			WebElement cityoption = cityoptions.get(i);
+			String cityname = cityoption.getText();
+
+			WebElement state = driver.findElement(By.id("FML_USR_ADDRSS_STTE_DIS"));
+
+			String statename = state.getAttribute("punjab");
+
+			System.out.println(cityname + "   ==>     " + statename);
+
+			if (state.equals("punjab")) {
 				System.out.println(cityname);
 			}
-			
-			}
-	    
-	    	    	
-  }
-  @BeforeMethod
-  public void beforeMethod() 
-  {
-	  System.setProperty("webdriver.gecko.driver", "D:\\drivers\\geckodriver-v0.24.0-win64\\geckodriver.exe");
-		WebDriver driver= new FirefoxDriver();
+
+		}
+
+	}
+	
+	@BeforeMethod
+	public void beforeMethod() {
+		System.setProperty("webdriver.gecko.driver", "D:\\drivers\\geckodriver-v0.24.0-win64\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
 		driver.get("http://content.icicidirect.com/idirectcontent/Home/Home.aspx");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-  }
-
-  @AfterMethod
-  public void afterMethod() 
-  {
-	  driver.quit();
-  }
+	}
+	
+	
 
 }
